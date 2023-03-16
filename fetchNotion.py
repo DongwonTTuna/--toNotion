06e7a70a7430e5e4fbd79114588f2f1029bec5f2
DongwonTuna <ttuna0790@gmail.com>
2023-03-16 14:04:13 +0900

    upload

--- fetchNotion.py
+++ fetchNotion.py
@@ -1,14 +1,12 @@
-from readCSV import getAllHoliday
 from notion_client import Client
 import pandas as pd
 import os, datetime
 
 def readTXTFile():
     lst = []
-    with open("keys/toRead.txt",'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            lst.append(line.strip('\n'))
+    for file in os.listdir("csv"):
+        if file.endswith('.csv'):
+            lst.append(file)
     return lst
 
 def getAllHoliday():
@@ -18,8 +16,10 @@ def getAllHoliday():
         res[item.split('.')[0]] = pd.read_csv(f"csv/{item}",names=["日付","曜日","名前"],header=None)
     return res
 
-API_KEY = os.environ["NOTION_TOKEN"]
-DATABASE_KEY, VIEW_ID = map(str, os.environ["TABLE_LINK"].replace(
+print("Notion API Keyを入力してください。")
+API_KEY = input()
+print("データベースのリンクを入力してください。")
+DATABASE_KEY, VIEW_ID = map(str, input().replace(
     'https://www.notion.so/', '').split('?v='))
 
 notion = Client(auth=API_KEY)
@@ -28,17 +28,19 @@ items = getAllHoliday()
 for item in items.values():
     df = pd.DataFrame(item)
     for index, item in df.iterrows():
-        print(item['日付'])
-        print(item['曜日'])
-        print(item['名前'])
+        
         date = date_obj = datetime.datetime.strptime(item['日付'], "%Y/%m/%d")
 
         new_page = {
             "名前": {"title": [{"text": {"content": item['名前']}}]},
             "日付": {"date": {"start": date_obj.date().isoformat()}},
-            "必要品目": {"rich_text": [{"text": {"content": item["名前"]}}]},
             "タグ": {"multi_select": [{"name": "祝日"}]}
         }
-
-        notion.pages.create(
+        try:
+            notion.pages.create(
             parent={"database_id": f"{DATABASE_KEY}"}, properties=new_page)
+        except Exception as err:
+            print(f"名前 : { item['名前']} 日付 : {item['日付']} を追加する途中にエラーが発生しました。")
+            print("エラー：")
+            print(err)
+        print(f"名前 : { item['名前']} 日付 : {item['日付']} がデータベースに追加されました ")
--- readme.md
+++ readme.md
@@ -7,10 +7,87 @@
 - NotionのAPIキーの発行
 - Python 3.8 ↑　
 - notion_client ライブラリ
-- 環境変数の設定
 
 <br/><br/>
 
-#### 手順
+# 手順
+## Syukujitsu.netから祝日のデータをダウンロード
+![syukujitsu.net](img/syukujitsu.png)
+<br/>
+ダウンロードしたファイルをcsvフォルダの中に入れておきます。
+<br/><br/><br/>
 
-NotionのAPIの発行
+## NotionAPIキーの発行
+https://www.notion.so/my-integrations に接続します。
+
+![createAPI](img/createNew.png)
+<br/>
+Create New Integrationをクリックします。
+<br/>
+![toSubmit](img/ToSubmit.png)
+<br/><br/><br/><br/>
+ Nameに 'For Holiday'を入力し、<br/>オプションとしてworkspaceを選択してあげます。
+<br/><br/><br/><br/>
+
+![APIkey](img/apikey.png)
+<br/>
+
+#### copyボタンを押して、`API_KEY`を他の誰かが見れない<br/>安全なところに貼り付けて保存しておきましょう。
+<br/><br/>
+
+
+##　データベースのリンクの取得
+![DBLink](img/viewLink.png)
+
+<br/>
+#### 後で`データベースのリンク`も使われますので、どこかに貼り付けておきましょう。
+<br/><br/>
+
+
+
+## NotionAPIに権限を付与
+##### 作ったNotionAPIにデータベースの修正の権限を付与します。
+![3point](img/3point.png)
+![toShare](img/toShare.png)
+<br/>
+「はい」を押して、次のステップへ移行します。
+<br/><br/>
+
+
+## Notionの設定
+##### 対象になるカレンダーのデータベースのレイアウトを変えていきます。
+<br/>
+
+![Calendar](img/selectLayout.png)
+<br/>
+カレンダービューの右の方にある...をクリックします。
+<br/>
+
+![Calendar2](img/tableview.png)
+<br/>
+テーブルを押します。
+<br/>
+
+![Tag](img/tags.png)
+<br/>
+「名前」、「タグ」、「日付」
+それぞれの列が存在するか、そして日本語で書いているかを確認します。
+存在しない場合、新しく作っておきましょう。
+
+<br/><br/>
+
+## notion_clientのライブラリーのインストール
+
+ターミナルまたはcmdを開き、`pip install notion-client`を入力します。
+<br/><br/>
+## 実行！
+
+```sh
+python [fetchNotionまでのパス]/fetchNotion.py
+```
+を実行し、先に保存しておいたAPI KEYとデータベースのリンクを貼り付けてあげます。
+
+![Terminal](img/terminal.png)
+
+上のように追加していくと終わりです！
+お疲れ様でした！
